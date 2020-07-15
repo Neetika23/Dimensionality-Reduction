46603e7eb591170b0c5ca2c4c4be3e7b324f65b8
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-15 17:54:58 +0530

    Create train_test.py

---
+++ Feature Selection 1/train_test.py
@@ -0,0 +1,13 @@
+# Import train_test_split()
+from sklearn.model_selection import train_test_split
+
+# Select the Gender column as the feature to be predicted (y)
+y = ansur_df['Gender']
+
+# Remove the Gender column to create the training data
+X = ansur_df.drop('Gender', axis = 1)
+
+# Perform a 70% train and 30% test data split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+print("{} rows in test set vs. {} in training set. {} Features.".format(X_test.shape[0], X_train.shape[0], X_test.shape[1]))
