c44e883a4b90e23da20614f7b3a06ea9fd2cdb5b
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-17 18:26:25 +0530

    Create nuclear.py

---
+++ Feature Selection 1/nuclear.py
@@ -0,0 +1,9 @@
+# Print the first five lines of weird_df
+print(weird_df.head())
+
+# Put nuclear energy production on the x-axis and the number of pool drownings on the y-axis
+sns.scatterplot(x='nuclear_energy', y='pool_drownings', data=weird_df)
+plt.show()
+
+# Print out the correlation matrix of weird_df
+print(weird_df.corr())
