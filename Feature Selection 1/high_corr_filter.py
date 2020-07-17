53aadb50d47d1fd0e75b150a61dfb82150377a5b
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-17 18:12:37 +0530

    Create high_corr_filter.py

---
+++ Feature Selection 1/high_corr_filter.py
@@ -0,0 +1,14 @@
+# Calculate the correlation matrix and take the absolute value
+corr_matrix = ansur_df.corr().abs()
+
+# Create a True/False mask and apply it
+mask = np.triu(np.ones_like(corr_matrix, dtype=bool))
+tri_df = corr_matrix.mask(mask)
+
+# List column names of highly correlated features (r > 0.95)
+to_drop = [c for c in tri_df.columns if any(tri_df[c] >  0.95)]
+
+# Drop the features in the to_drop list
+reduced_df = ansur_df.drop(to_drop, axis=1)
+
+print("The reduced dataframe has {} columns.".format(reduced_df.shape[1]))
