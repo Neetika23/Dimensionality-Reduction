bc98fa8d0175306a6d92430f0e1af551a24582e2
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-19 15:32:38 +0530

    Create lasso.py

---
+++ Feature Selection 2/lasso.py
@@ -0,0 +1,11 @@
+# Set the test size to 30% to get a 70-30% train test split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# Fit the scaler on the training features and transform these in one go
+X_train_std = scaler.fit_transform(X_train)
+
+# Create the Lasso model
+la = Lasso()
+
+# Fit it to the standardized training data
+la.fit(X_train_std,y_train)
