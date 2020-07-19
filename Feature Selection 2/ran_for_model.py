b6d750cbfb066841748bc2e751b83c8ccdb2e185
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-19 15:13:33 +0530

    Create ran_for_model.py

---
+++ Feature Selection 2/ran_for_model.py
@@ -0,0 +1,15 @@
+# Perform a 75% training and 25% test data split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+
+# Fit the random forest model to the training data
+rf = RandomForestClassifier(random_state=0)
+rf.fit(X_train, y_train)
+
+# Calculate the accuracy
+acc = accuracy_score(y_test, rf.predict(X_test))
+
+# Print the importances per feature
+print(dict(zip(X.columns, rf.feature_importances_.round(2))))
+
+# Print accuracy
+print("{0:.1%} accuracy on test set.".format(acc))
