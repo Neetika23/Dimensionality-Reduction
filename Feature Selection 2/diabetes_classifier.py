bf393063b15d8cc95f9f28c14f62399d182759fa
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-19 14:54:08 +0530

    Create diabetes_classifier.py

---
+++ Feature Selection 2/diabetes_classifier.py
@@ -0,0 +1,15 @@
+# Fit the scaler on the training features and transform these in one go
+X_train_std = scaler.fit_transform(X_train)
+
+# Fit the logistic regression model on the scaled training data
+lr.fit(X_train_std, y_train)
+
+# Scale the test features
+X_test_std = scaler.transform(X_test)
+
+# Predict diabetes presence on the scaled test set
+y_pred = lr.predict(X_test_std)
+
+# Prints accuracy metrics and feature coefficients
+print("{0:.1%} accuracy on test set.".format(accuracy_score(y_test, y_pred))) 
+print(dict(zip(X.columns, abs(lr.coef_[0]).round(2))))
