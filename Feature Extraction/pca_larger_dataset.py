d8064987b9b363237a5116176320437c87e84b62
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-20 12:42:00 +0530

    Create pca_larger_dataset.py

---
+++ Feature Extraction/pca_larger_dataset.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# Scale the data
+scaler = StandardScaler()
+ansur_std = scaler.fit_transform(ansur_df)
+
+# Apply PCA
+pca = PCA()
+pca.fit(ansur_std)
+
+# Print the cumulative sum of the explained variance ratio
+print(pca.explained_variance_ratio_.cumsum())
