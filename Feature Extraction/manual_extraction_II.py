2d762af2944f486041cb8880b62ea19b530dc779
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-20 12:12:32 +0530

    Create manual_extraction_II.py

---
+++ Feature Extraction/manual_extraction_II.py
@@ -0,0 +1,7 @@
+# Calculate the mean height
+height_df['height'] = height_df[['height_1', 'height_2', 'height_3']].mean(axis=1)
+
+# Drop the 3 original height features
+reduced_df = height_df.drop(['height_1', 'height_2', 'height_3'], axis=1)
+
+print(reduced_df.head())
