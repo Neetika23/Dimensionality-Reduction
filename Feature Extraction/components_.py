299fc1044453394e95791d4aef965e2c5e4bae25
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-20 13:05:43 +0530

    Create components_.py

---
+++ Feature Extraction/components_.py
@@ -0,0 +1,11 @@
+# Build the pipeline
+pipe = Pipeline([('scaler', StandardScaler()),
+        		 ('reducer', PCA(n_components=2))])
+
+# Fit it to the dataset and extract the component vectors
+pipe.fit(poke_df)
+vectors = pipe.steps[1][1].components_.round(2)
+
+# Print feature effects
+print('PC 1 effects = ' + str(dict(zip(poke_df.columns, vectors[0]))))
+print('PC 2 effects = ' + str(dict(zip(poke_df.columns, vectors[1]))))
