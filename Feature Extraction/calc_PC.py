8aef8594fc2a2ba34285f6010d7904877fea269f
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-20 12:36:36 +0530

    Create calc_PC.py

---
+++ Feature Extraction/calc_PC.py
@@ -0,0 +1,20 @@
+# Create a pairplot to inspect ansur_df
+sns.pairplot(ansur_df)
+
+plt.show()
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# Create the scaler
+scaler = StandardScaler()
+ansur_std = scaler.fit_transform(ansur_df)
+
+# Create the PCA instance and fit and transform the data with pca
+pca = PCA()
+pc = pca.fit_transform(ansur_std)
+pc_df = pd.DataFrame(pc, columns=['PC 1', 'PC 2', 'PC 3', 'PC 4'])
+
+# Create a pairplot of the principal component dataframe
+sns.pairplot(pc_df)
+plt.show()
