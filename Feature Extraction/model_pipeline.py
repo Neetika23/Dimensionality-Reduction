a1bf35a7f1c9127214c563a28beb3fe9b03227cd
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-20 13:12:09 +0530

    Create model_pipeline.py

---
+++ Feature Extraction/model_pipeline.py
@@ -0,0 +1,14 @@
+# Build the pipeline
+pipe = Pipeline([
+        ('scaler', StandardScaler()),
+        ('reducer', PCA(n_components=2)),
+        ('classifier', RandomForestClassifier(random_state=0))])
+
+# Fit the pipeline to the training data
+pipe.fit(X_train, y_train)
+
+# Score the accuracy on the test set
+accuracy = pipe.score(X_test,y_test)
+
+# Prints the model accuracy
+print('{0:.1%} test set accuracy'.format(accuracy))
